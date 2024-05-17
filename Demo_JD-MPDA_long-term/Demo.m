0919e71cab83796b355e532cc836a1cac3f4cb30
topaurora <110376585+topaurora@users.noreply.github.com>
2024-05-17 10:59:31 +0800

    Update Demo.m

--- Demo_JD-MPDA_long-term/Demo.m
+++ Demo_JD-MPDA_long-term/Demo.m
@@ -5,25 +5,31 @@
 clc
 clear
 load Drift_dataset
-XS = Drift_dataset{1,1};
 % Use batch 1 as source domain
-ys = Drift_dataset{1,2};
+XS = Drift_dataset{1,1};
 % labels of batch 1
-d_m = [2 4 16 4 4 4 128 2 4]; 
-sig_ma = [1 30 30 30 10 20 1 10 20];
-yit_a = [10 30 20 30 10 1 1 1 1];
-gam_ma = [60 60 1 40 20 40 40 60 30];
-k_c = [4 4 8 8 8 8 16 4 8];
-k_p = [40 50 50 40 50 40 60 40 40];
-k_t = [60 30 30 30 30 50 50 30 60];
-yib_sio = [0.5 0.6 0.8 0.8 0 0 0 0.2 0.1];
+ys = Drift_dataset{1,2};
+
+%parameters
+d_m = [4 4 4 4 4 2 4 2 4];
+sig_ma = [20 40 30 20 10 40 10 30 4];
+yit_a = [1 46 10 30 10 30 20 40 22];
+gam_ma = [1 42 20 1 20 10 10 20 32];
+k_c = [32 8 32 32 8 32 2 32 16];
+k_p = [40 30 50 40 50 40 60 40 30];
+k_t = [30 10 60 30 30 50 40 50 10];
+yib_sio = [0.4 0.2 0.6 0 0 0.3 0.1 0.5 0.4];
+sita = [0.6 0.3 0.4 0.6 1 0.6 0.6 0.6 0.5];
 tow = 10;
-%parameters 
-predict_acc = cell(9,2);
-% Store the highest classification accuracy.
+c = 1;
+g = [0.25 0.25 0.25 0.25 0.25 0.5 0.25 0.5 0.25];
+predict_acc = cell(9,1);
+
+
 for i = 1:9
-    XT = Drift_dataset{i+1,1}; % Use batch 2-10 as target domain, respectively
-    yt = Drift_dataset{i+1,2}; % labels
+    XT = Drift_dataset{i+1,1};
+    yt = Drift_dataset{i+1,2};
+    nt = size(XT,2);
     d = d_m(i);
     sig = sig_ma(i);
     yit = yit_a(i);
@@ -32,35 +38,98 @@ for i = 1:9
     kp = k_p(i);
     kt = k_t(i);
     yib = yib_sio (i);
-    accuracy = 0; % Reset  accuracy per batch to avoid overlap
+    sit = sita(i);
+    accuracy = 0; % max accuracy
     
 %% Model    
-    [Gc,Gp,Gt] = calculation(XS,XT,ys,kc,kp,kt,tow); % Calculate the portion of the iteration that does not change
+    [Gc,Gp,Gt] = calculation_MPDA(XS,XT,ys,kc,kp,kt,tow); % Calculate the portion of the iteration that does not change
+    
     CDD = zeros(128,1); % Initialising the conditional distribution
-    for r = 1:100
-        [XS_new, XT_new] = reduction(XS,XT,CDD,d,sig,yit,yib,gam,Gc,Gp,Gt);
+
+    for r = 1:10 
+        [XS_new, XT_new,P] = reduction(XS,XT,CDD,d,sig,yit,yib,gam,Gc,Gp,Gt);
         
-%%     SVM libsvm was developed by Professor Lin of National Taiwan University and is publicly available.     
+%% SVM libsvm was developed by Professor Lin of National Taiwan University and is publicly available.     
         X_train = zscore(XS_new');
         X_test = zscore(XT_new');
         y_train = ys;
         y_test = yt;
-% To verify the effectiveness of the model in removing drift, we use a standard SVM for classification, with default values for all parameters
-        cmd = [' -t 2',' -c ',num2str(1),' -g ',num2str(1/d)];
+        bestc = c;
+        bestg = g(i);
+        
+        if sit ~= 1  % sita == 1 means skip the screening step
+            cmd = [' -c ',num2str(bestc),' -g ',num2str(bestg),' -b 1 '];
+            model = svmtrain(y_train,X_train,cmd); 
+            [predict_label,~,dec_values] = svmpredict(y_test,X_test,model,'-b 1');
 
-        model = svmtrain(y_train,X_train,cmd);
-        [predict_label,acc_predict,~] = svmpredict(y_test,X_test,model);
-% Calculate classification accuracy of target data in subspace.
-        acc_test = length(find(predict_label == y_test))/length(y_test);
-        if accuracy < acc_test
-            accuracy = acc_test; 
-            acce = acd(1);
+        else
+            cmd = [' -c ',num2str(bestc),' -g ',num2str(bestg)];
+            model = svmtrain(y_train,X_train,cmd);
+            [predict_label,~,dec_values] = svmpredict(y_test,X_test,model);
+            
         end
-% Update the conditional distribution distance
-        [distance] = iteration_CDD(XS,XT,ys,predict_label); 
+        
+        acc = length(find(predict_label == y_test))/length(y_test); % accuracy is determined by dividing the number of correctly predicted labels by the total number.
+        if accuracy < acc 
+            accuracy = acc;
+        end
+
+%% Calculate the conditional-entropy loss
+
+        if sit ~= 1
+            
+            pkx_total = sum(exp(dec_values),2); 
+            
+            probability_exp = exp(dec_values); 
+            
+            X_exp = probability_exp ./ pkx_total; 
+            
+            X_exp =-1 * X_exp .* log(X_exp);  
+            
+            Loss_ent = sum(X_exp,2);         
+            
+            Loss_ent = mapminmax(Loss_ent',0, 1); % normalize the valus to [0,1]
+            
+%% Selection
+
+            t = 1;                              
+            
+            yt_new = zeros(nt,1);  
+            
+            for lt = 1: nt      
+            
+                if  Loss_ent(lt) < sit          
+                
+                    yt_new(lt,1) = predict_label(lt);
+                    
+                    t = t + 1;
+                
+                end
+                
+            end
+  
+        else
+            
+            yt_new = predict_label;
+        
+        end
+        
+        [distance] = iteration_CDD(XS,XT,ys,yt_new); 
+        
         CDD = distance;
-                                        
+    
     end
-    predict_acc{i,1} = acce; 
-    predict_acc{i,2} = accuracy;
+    
+    predict_acc{i,1} = accuracy;
+
 end
+
+
+pause(0.2);
+clc
+for j = 1:9
+    disp(['Source domain: Batch',num2str(1),', Target domain: Batch',num2str(j+1),', ','Accuracy = ' num2str(predict_acc{j,1}*100,'%.2f'),'%']);
+end
+        
+        
+    
