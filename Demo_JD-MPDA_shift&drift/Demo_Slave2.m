ca49529b04e97f537aad2d7090bd74b22ba69153
topaurora <110376585+topaurora@users.noreply.github.com>
2024-05-17 10:58:42 +0800

    Update Demo_Slave2.m

--- Demo_JD-MPDA_shift&drift/Demo_Slave2.m
+++ Demo_JD-MPDA_shift&drift/Demo_Slave2.m
@@ -8,6 +8,7 @@ ys = master(:,1);
 XT = slave2(:,2:7);
 yt = slave2(:,1);
 XT =XT';
+
 % parameter
 d = 6;
 sig = 80;
@@ -15,33 +16,57 @@ yit = 10;
 gam = 20;
 kc = 8;
 kp = 70;
-
 kt = 60;
-accuracy = 0;
 yib = 0.5;
+bestc = 1;
+bestg = 0.1667;
+accuracy = 0;
+
 %% Model
-[Gc,Gp,Gt] = calculation(XS,XT,ys,kc,kp,kt,10);
+[Gc,Gp,Gt] = calculation_MPDA(XS,XT,ys,kc,kp,kt,10);
 
     CDD = zeros(6,1);
-    for r = 1:100
-        [XS_new,XT_new] = reduction(XS,XT,CDD,d,sig,yit,yib,gam,Gc,Gp,Gt);
-        data = zscore(XS_new');
-        data2 = zscore(XT_new');
-        X_train = data;
-        X_test = data2;
-        y_train = ys;
-        y_test = yt;
+for r = 1:100
 
+    [XS_new,XT_new] = reduction(XS,XT,CDD,d,sig,yit,yib,gam,Gc,Gp,Gt);
+    
+    data = zscore(XS_new');
+    
+    data2 = zscore(XT_new');
+    
+    X_train = data;
+    
+    X_test = data2;
+    
+    y_train = ys;
+    
+    y_test = yt;
+
+    
 %% SVM        
-         cmd = [' -t 2',' -c ',num2str(1),' -g ',num2str(1/d)];
+    
+    cmd = [' -t 2',' -c ',num2str(bestc),' -g ',num2str(bestg)];
 
-        model = svmtrain(y_train,X_train,cmd);
-        [predict_label,acc,~] = svmpredict(y_test,X_test,model);
-        acc_test = length(find(predict_label == y_test))/length(y_test);
-        if accuracy < acc_test
-            accuracy = acc_test;
+    
+    model = svmtrain(y_train,X_train,cmd);
+    
+    [predict_label,acc,~] = svmpredict(y_test,X_test,model);
+    
+    acc_test = length(find(predict_label == y_test))/length(y_test);  % accuracy is determined by dividing the number of correctly predicted labels by the total number.
+    
+    clc
+    
+    if accuracy < acc_test
+    
+        accuracy = acc_test;
 
-        end
-        [distance] = iteration_CQU_CDD(XS,XT,ys,predict_label);
-        CDD = distance;
     end
+    
+    [distance] = iteration_CQU_CDD(XS,XT,ys,predict_label);
+    
+    CDD = distance;
+    
+end
+    
+clc
+disp(['Source domain: Batch Master',', Target domain: Batch Slave ',num2str(2),', ','Accuracy = ' num2str(accuracy*100,'%.2f'),'%']);
