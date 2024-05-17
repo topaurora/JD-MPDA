39e18b1efaf218082b344081027b0d446f01a90b
topaurora <110376585+topaurora@users.noreply.github.com>
2024-05-17 10:58:06 +0800

    Update Demo_Slave1.m

--- Demo_JD-MPDA_shift&drift/Demo_Slave1.m
+++ Demo_JD-MPDA_shift&drift/Demo_Slave1.m
@@ -8,41 +8,94 @@ ys = master(:,1);
 XT = slave1(:,2:7);
 yt = slave1(:,1);
 XT =XT';
+nt = size(XT,2);
 % parameter 
 d = 6;
 sig = 80;
-yit = 10;
+yit = 90;
 gam = 10;
-kc = 32;
+kc = 16;
 kp = 70;
-kt = 50;
+kt = 60;
 accuracy = 0;
-yib = 0.6;
+yib = 0.1;
+sit = 0.8;
+c = 1;
+g = 0.1667;
 
 %% Model
 % Calculate the portion of the iteration that does not change
-[Gc,Gp,Gt] = calculation(XS,XT,ys,kc,kp,kt,10);
+[Gc,Gp,Gt] = calculation_MFDA(XS,XT,ys,kc,kp,kt,10);
 
     CDD = zeros(6,1);
-    for r = 1:100 % the cycle
-        [XS_new,XT_new] = reduction(XS,XT,CDD,d,sig,yit,yib,gam,Gc,Gp,Gt); % get subspace data
-        X_train = zscore(XS_new');
-        X_test = zscore(XT_new');
-        y_train = ys;
-        y_test = yt;
+for r = 1:100 % the cycle
 
+    [XS_new,XT_new,P] = reduction(XS,XT,CDD,d,sig,yit,yib,gam,Gc,Gp,Gt); % get subspace data
+    
+    X_train = zscore(XS_new');
+    
+    X_test = zscore(XT_new');
+    
+    y_train = ys;
+    
+    y_test = yt;
+
+    
 %% SVM
-        cmd = [' -t 2',' -c ',num2str(1),' -g ',num2str(1/d)];
+    
+    cmd = [' -t 2',' -c ',num2str(c),' -g ',num2str(g),' -b 1 '];
 
-        model = svmtrain(y_train,X_train,cmd);
-        [predict_label,acc,~] = svmpredict(y_test,X_test,model);
-        acc_test = length(find(predict_label == y_test))/length(y_test);
-        if accuracy < acc_test
-            accuracy = acc_test;
-
-        end
+    model = svmtrain(y_train,X_train,cmd);
+    
+    [predict_label,acc,dec_values] = svmpredict(y_test,X_test,model,'-b 1');
+    
+    acc_test = length(find(predict_label == y_test))/length(y_test); % accuracy is determined by dividing the number of correctly predicted labels by the total number.
+    
+    if accuracy < acc_test
+    
+        accuracy = acc_test;
+        
+    end
 % Update the conditional distribution distance
-        [distance] = iteration_CQU_CDD(XS,XT,ys,predict_label); 
-        CDD = distance;
+  
+    pkx_total = sum(exp(dec_values),2); 
+            
+    probability_exp = exp(dec_values); 
+            
+    X_exp = probability_exp ./ pkx_total; 
+            
+    X_exp =-1 * X_exp .* log(X_exp);      
+    
+    Loss_ent = sum(X_exp,2);              % Calculate the conditional-entropy loss 
+           
+    Loss_ent = mapminmax(Loss_ent',0, 1); % normalize the valus to [0,1]
+    
+% Selection   
+    
+    t = 1;                               
+    
+    yt_new = zeros(nt,1);  
+       
+    
+    for lt = 1: nt      
+     
+        if  Loss_ent(lt) < sit          
+         
+            yt_new(lt,1) = predict_label(lt);
+          
+            t = t + 1;
+            
+        end
+        
     end
     
+    [distance] = iteration_CQU_CDD(XS,XT,ys,yt_new);
+    
+    CDD = distance;
+    
+end
+
+pause(0.2);
+clc
+
+disp(['Source domain: Batch Master',', Target domain: Batch Slave ',num2str(1),', ','Accuracy = ' num2str(accuracy*100,'%.2f'),'%']);
